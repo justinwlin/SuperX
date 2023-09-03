cec1d83cdae2ccd05592d50883e75c009c8ad472
Justin Lin <justinwlin@gmail.com>
2023-09-03 14:12:24 -0400

    Updating index.ts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,8 +30,8 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`FAANG | ${siteConfig.title}`}
+      description="How to get into FAANG">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
